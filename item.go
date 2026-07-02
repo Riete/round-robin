6083cf6e7720f4c1398b42c2055c8cb709b56c26
riet <loli_520@126.com>
2026-07-02 18:21:27 +0800

    update WeightedItem

--- item.go
+++ item.go
@@ -8,12 +8,16 @@ type WeightedItem[T any] struct {
 	totalWeight int64
 }
 
-func (w *WeightedItem[T]) Identity() ID {
+func (w *WeightedItem[T]) ID() ID {
 	return w.id
 }
 
 func (w *WeightedItem[T]) Item() T {
 	return w.item
+}
+
+func (w *WeightedItem[T]) Weight() int64 {
+	return w.weight
 }
 
 func (w *WeightedItem[T]) CurrentWeight() int64 {
@@ -22,4 +26,8 @@ func (w *WeightedItem[T]) CurrentWeight() int64 {
 
 func (w *WeightedItem[T]) NextWeight() int64 {
 	return w.CurrentWeight() - w.totalWeight
+}
+
+func (w *WeightedItem[T]) TotalWeight() int64 {
+	return w.totalWeight
 }
